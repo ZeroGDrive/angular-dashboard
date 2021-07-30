7a3e73016915999cf7020985a3a8aa28541b47fb
Ayoub Alfurjani <a.alfurjani@muljin.com>
2021-07-30 20:20:59 +0200

    made some improvements

--- src/app/core/layout/sign-in/sign-in.component.ts
+++ src/app/core/layout/sign-in/sign-in.component.ts
@@ -1,16 +1,24 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignInComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _fb: FormBuilder) {
+    this.loginForm = _fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
   }
 
+  ngOnInit(): void {}
 }
--- src/app/core/layout/sign-up/sign-up.component.ts
+++ src/app/core/layout/sign-up/sign-up.component.ts
@@ -1,50 +1,64 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {of, Subject} from "rxjs";
-import {delay, finalize, tap} from "rxjs/operators";
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { of, Subject } from 'rxjs';
+import { delay, finalize, tap, share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignUpComponent implements OnInit {
   registerForm: FormGroup;
   private _formError = new Subject<boolean>();
-  formError = this._formError.asObservable();
+  formError = this._formError.asObservable().pipe(share());
 
   constructor(private _fb: FormBuilder) {
     this.registerForm = _fb.group({
       storeName: new FormControl('', Validators.required),
       storeType: new FormControl('', Validators.required),
       adminName: new FormControl('', Validators.required),
-      phoneNumber: new FormControl('', [Validators.required, Validators.minLength(8),
-        Validators.maxLength(8)]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(8),
+      ]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)])
-    })
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
   }
 
   storeType = [
-    {label: 'فرد', icon: 'pi-user', key: 1},
-    {label: 'مؤسسة', icon: 'pi-users', key: 2},
-    {label: 'شركة', icon: 'pi-briefcase', key: 3}
+    { label: 'فرد', icon: 'pi-user', key: 1 },
+    { label: 'مؤسسة', icon: 'pi-users', key: 2 },
+    { label: 'شركة', icon: 'pi-briefcase', key: 3 },
   ];
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   checkForm(): void {
-    of(null).pipe(
-      tap(() => this._formError.next(true)),
-      delay(3000),
-      finalize(() => this._formError.next(false))
-    ).subscribe();
+    of(null)
+      .pipe(
+        tap(() => this._formError.next(true)),
+        delay(3000),
+        finalize(() => this._formError.next(false))
+      )
+      .subscribe();
   }
 
   catchFormErrors(controlName: string, errorType: string): boolean {
-    return this.registerForm.invalid && this.registerForm.controls[controlName].getError(errorType);
+    return (
+      this.registerForm.invalid &&
+      this.registerForm.controls[controlName].getError(errorType)
+    );
   }
-
 }
